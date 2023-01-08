4b8e8f568fcfc0772c5f8d35352647aa65c85d0e
Yuuma-Ujimoto <yxy.yuuma@gmail.com>
2023-01-08 21:55:01 +0900

    feat:Formの挙動調査2

--- src/views/FormTest.vue
+++ src/views/FormTest.vue
@@ -1,11 +1,11 @@
 <template>
-  <form action="" >
+  <form @submit.prevent="submit" >
     <dl>
       <dd>
-        <input v-model="Form.text" type="text" name="test">
+        <input v-model="form.text" type="text" name="test">
       </dd>
       <dd>
-        <button @click="submit">送信</button>
+        <button type="submit">送信</button>
       </dd>
     </dl>
   </form>
@@ -16,14 +16,21 @@ export default {
   // form にv-modelは使えない
   // エラー文：Internal server error: v-model can only be used on <input>, <textarea> and <select> elements.
   // 意訳：input,textarea,select以外でv-model使うな
+
+  // object型のDataを扱う際はkeyが予め登録された値じゃないとv-modelは適応できない
+
   name: "FormTest",
   data(){
   return{
-    form:null
+    form:{
+      text:""
+    }
   }
   },
   methods:{
   submit(){
+    // formタグ使う際はButtonのonClickイベントの発火でメソッドを呼び出すのではなくformのonSubmit.preventを呼び出したほうがいい
+    // preventを使うと本来発生するイベントの挙動を無視してmethodを呼び出す(onSubmitではactionの指定Pathにリダイレクトする等)
     console.log(this.form)
   }
   }
